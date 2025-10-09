15f5ba8cf7ac743b06cdb677b7f2751b8a6a6084
Robin Erhardt <67264436+RobinErhardt@users.noreply.github.com>
2025-10-09 17:32:12 +0200

    Added some tests.

---
+++ eigene-code-vorlesung/2025-10-09/datatypes/datatypes_test.go
@@ -0,0 +1,22 @@
+package datatypes
+
+import "fmt"
+
+func Exapmle_various_data_types() {
+	var i1 int
+	i2 := 55
+
+	fmt.Println(i1)
+	fmt.Println(i2)
+
+	var f1 float64
+	f2 := 3.0 / 4.0
+
+	fmt.Println(f1)
+	fmt.Println(f2)
+
+	// Output:
+	// 0
+	// 0.75
+
+}
---
+++ eigene-code-vorlesung/2025-10-09/datatypes/integers_test.go
@@ -0,0 +1,15 @@
+package datatypes
+
+import "fmt"
+
+func Example_integers() {
+	var i int
+	k := 55
+
+	fmt.Println(i)
+	fmt.Println(k)
+
+	// Output:
+	// 0
+	// 55
+}
